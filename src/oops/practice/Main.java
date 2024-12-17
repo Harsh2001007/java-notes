8687bd07794f7e591d37007fc0d0afc266eeb7b6
Harsh <hs5686584@gmail.com>
2024-12-17 22:59:55 +0530

    inheritance done

---
+++ src/oops/practice/Main.java
@@ -0,0 +1,29 @@
+package oops.practice;
+
+class Employee {
+    String name;
+    int age;
+
+    Employee(String name) {
+        this.name = name; // Referring to the current object's `name`
+    }
+
+    Employee(int age) {
+        this.age = age; // Referring to the current object's `name`
+    }
+
+
+
+    void introduce() {
+        System.out.println("Hi, I am " + this.name);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Employee emp = new Employee("Alice");
+
+
+        emp.introduce(); // Output: Hi, I am Alice
+    }
+}

327ec0c509ab6c779a0738067017099a47c35bc8
Harsh Sachan <87149088+Harsh2001007@users.noreply.github.com>
2025-01-21 20:45:08 +0530

    Update Bye_world.java

--- src/Bye_world.java
+++ src/Bye_world.java
@@ -2,6 +2,6 @@ class bye_world{
     public static void main(String args[]){
         System.out.println("Bye World");
         System.out.print("this is line one");
-        System.out.println("this is line two");
+        System.out.println("this one is line two");
     }
 }
